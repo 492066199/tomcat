7f0223372026d1b3ca39c5ec3909245a4fdbd6fe
sailingYang <492066199@qq.com>
2018-06-09 23:10:31 +0800

    add request method impl

--- src/main/java/com/sailing/tomcat/servlet/HttpRequest.java
+++ src/main/java/com/sailing/tomcat/servlet/HttpRequest.java
@@ -1,14 +1,16 @@
 package com.sailing.tomcat.servlet;
 
 import com.sailing.tomcat.http.ParameterMap;
+import com.sailing.tomcat.io.RequestStream;
+import com.sailing.tomcat.util.Enumerator;
+import com.sailing.tomcat.util.RequestUtil;
 
 import javax.servlet.*;
 import javax.servlet.http.*;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.UnsupportedEncodingException;
+import java.io.*;
 import java.security.Principal;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.*;
 
 public class HttpRequest implements HttpServletRequest{
@@ -16,8 +18,8 @@ public class HttpRequest implements HttpServletRequest{
     private InputStream input;
 
     //
-    protected HashMap headers = new HashMap();
-    protected ArrayList cookies = new ArrayList();
+    protected HashMap<String, Object> headers = new HashMap();
+    protected ArrayList<Cookie> cookies = new ArrayList();
     protected ParameterMap parameters = null;
 
     private String requestedSessionId;
@@ -40,32 +42,94 @@ public class HttpRequest implements HttpServletRequest{
         this.input = input;
     }
 
+    public InputStream getInput() {
+        return input;
+    }
+
     public String getAuthType() {
         return null;
     }
 
     public Cookie[] getCookies() {
-        return new Cookie[0];
+        synchronized (cookies) {
+            if (cookies.size() < 1)
+                return (null);
+            Cookie results[] = new Cookie[cookies.size()];
+            return ((Cookie[]) cookies.toArray(results));
+        }
     }
 
-    public long getDateHeader(String s) {
-        return 0;
+    /**
+     * The set of SimpleDateFormat formats to use in getDateHeader().
+     */
+    protected SimpleDateFormat formats[] = {
+            new SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss zzz", Locale.US),
+            new SimpleDateFormat("EEEEEE, dd-MMM-yy HH:mm:ss zzz", Locale.US),
+            new SimpleDateFormat("EEE MMMM d HH:mm:ss yyyy", Locale.US)
+    };
+
+    public long getDateHeader(String name) {
+        String value = getHeader(name);
+        if (value == null)
+            return (-1L);
+
+        // Work around a bug in SimpleDateFormat in pre-JDK1.2b4
+        // (Bug Parade bug #4106807)
+        value += " ";
+
+        // Attempt to convert the date header in a variety of formats
+        for (int i = 0; i < formats.length; i++) {
+            try {
+                Date date = formats[i].parse(value);
+                return (date.getTime());
+            }
+            catch (ParseException e) {
+                ;
+            }
+        }
+        throw new IllegalArgumentException(value);
     }
 
-    public String getHeader(String s) {
-        return null;
+    public String getHeader(String name) {
+        name = name.toLowerCase();
+        synchronized (headers) {
+            ArrayList values = (ArrayList) headers.get(name);
+            if (values != null)
+                return ((String) values.get(0));
+            else
+                return null;
+        }
     }
 
-    public Enumeration<String> getHeaders(String s) {
-        return null;
+    protected static ArrayList empty = new ArrayList();
+
+    public Enumeration<String> getHeaders(String name) {
+        name = name.toLowerCase();
+        synchronized (headers) {
+            ArrayList values = (ArrayList) headers.get(name);
+            if (values != null)
+                return (new Enumerator(values));
+            else
+                return (new Enumerator(empty));
+        }
     }
 
     public Enumeration<String> getHeaderNames() {
-        return null;
+        synchronized (headers) {
+            Vector<String> r = new Vector<String>();
+            for(String key : headers.keySet()){
+                r.add(key);
+            }
+            return r.elements();
+        }
     }
 
-    public int getIntHeader(String s) {
-        return 0;
+    public int getIntHeader(String name) {
+        String value = getHeader(name);
+        if (value == null)
+            return (-1);
+        else
+            return (Integer.parseInt(value));
     }
 
     public String getMethod() {
@@ -80,8 +144,17 @@ public class HttpRequest implements HttpServletRequest{
         return null;
     }
 
+
+    protected String contextPath = "";
+    public void setContextPath(String path) {
+        if (path == null)
+            this.contextPath = "";
+        else
+            this.contextPath = path;
+    }
+
     public String getContextPath() {
-        return null;
+        return contextPath;
     }
 
     public String getQueryString() {
@@ -160,12 +233,27 @@ public class HttpRequest implements HttpServletRequest{
         return null;
     }
 
-    public Object getAttribute(String s) {
-        return null;
+
+    /**
+     * The request attributes for this request.
+     */
+    protected HashMap attributes = new HashMap();
+
+    public Object getAttribute(String name) {
+        synchronized (attributes) {
+            return (attributes.get(name));
+        }
     }
 
     public Enumeration<String> getAttributeNames() {
-        return null;
+        synchronized (attributes) {
+            Vector<String> rs = new Vector<String>();
+            for(Object key : attributes.keySet()){
+                rs.add((String) key);
+            }
+
+            return rs.elements();
+        }
     }
 
     public String getCharacterEncoding() {
@@ -177,31 +265,148 @@ public class HttpRequest implements HttpServletRequest{
     }
 
     public int getContentLength() {
-        return 0;
+        return this.contentLength;
     }
 
     public String getContentType() {
-        return null;
+        return contentType;
+    }
+
+    /**
+     * The reader that has been returned by <code>getReader</code>, if any.
+     */
+    protected BufferedReader reader = null;
+
+    /**
+     * The ServletInputStream that has been returned by
+     * <code>getInputStream()</code>, if any.
+     */
+    protected ServletInputStream stream = null;
+
+    /**
+     * Create and return a ServletInputStream to read the content
+     * associated with this Request.  The default implementation creates an
+     * instance of RequestStream associated with this request, but this can
+     * be overridden if necessary.
+     *
+     * @exception IOException if an input/output error occurs
+     */
+    public ServletInputStream createInputStream() throws IOException {
+        return (new RequestStream(this));
     }
 
     public ServletInputStream getInputStream() throws IOException {
-        return null;
+        if (reader != null)
+            throw new IllegalStateException("getInputStream has been called");
+
+        if (stream == null)
+            stream = createInputStream();
+        return (stream);
     }
 
-    public String getParameter(String s) {
-        return null;
+
+    /**
+     * Have the parameters for this request been parsed yet?
+     */
+    protected boolean parsed = false;
+
+
+    /**
+     * Parse the parameters of this request, if it has not already occurred.
+     * If parameters are present in both the query string and the request
+     * content, they are merged.
+     */
+    protected void parseParameters() {
+        if (parsed)
+            return;
+        ParameterMap results = parameters;
+        if (results == null)
+            results = new ParameterMap();
+        results.setLocked(false);
+        String encoding = getCharacterEncoding();
+        if (encoding == null)
+            encoding = "ISO-8859-1";
+
+        // Parse any parameters specified in the query string
+        String queryString = getQueryString();
+        try {
+            RequestUtil.parseParameters(results, queryString, encoding);
+        }
+        catch (UnsupportedEncodingException e) {
+            ;
+        }
+
+        // Parse any parameters specified in the input stream
+        String contentType = getContentType();
+        if (contentType == null)
+            contentType = "";
+        int semicolon = contentType.indexOf(';');
+        if (semicolon >= 0) {
+            contentType = contentType.substring(0, semicolon).trim();
+        }
+        else {
+            contentType = contentType.trim();
+        }
+        if ("POST".equals(getMethod()) && (getContentLength() > 0)
+                && "application/x-www-form-urlencoded".equals(contentType)) {
+            try {
+                int max = getContentLength();
+                int len = 0;
+                byte buf[] = new byte[getContentLength()];
+                ServletInputStream is = getInputStream();
+                while (len < max) {
+                    int next = is.read(buf, len, max - len);
+                    if (next < 0 ) {
+                        break;
+                    }
+                    len += next;
+                }
+                is.close();
+                if (len < max) {
+                    throw new RuntimeException("Content length mismatch");
+                }
+                RequestUtil.parseParameters(results, buf, encoding);
+            }
+            catch (UnsupportedEncodingException ue) {
+                ;
+            }
+            catch (IOException e) {
+                throw new RuntimeException("Content read fail");
+            }
+        }
+
+        // Store the final results
+        results.setLocked(true);
+        parsed = true;
+        parameters = results;
+    }
+
+    public String getParameter(String name) {
+        parseParameters();
+        String values[] = (String[]) parameters.get(name);
+        if (values != null)
+            return (values[0]);
+        else
+            return (null);
     }
 
     public Enumeration<String> getParameterNames() {
-        return null;
+        parseParameters();
+        return (new Enumerator(parameters.keySet()));
     }
 
-    public String[] getParameterValues(String s) {
-        return new String[0];
+    public String[] getParameterValues(String name) {
+        parseParameters();
+        String values[] = (String[]) parameters.get(name);
+        if (values != null)
+            return (values);
+        else
+            return null;
     }
 
     public Map<String, String[]> getParameterMap() {
-        return null;
+        parseParameters();
+        return (this.parameters);
     }
 
     public String getProtocol() {
@@ -221,7 +426,17 @@ public class HttpRequest implements HttpServletRequest{
     }
 
     public BufferedReader getReader() throws IOException {
-        return null;
+        if (stream != null)
+            throw new IllegalStateException("getInputStream has been called.");
+        if (reader == null) {
+            String encoding = getCharacterEncoding();
+            if (encoding == null)
+                encoding = "ISO-8859-1";
+            InputStreamReader isr =
+                    new InputStreamReader(createInputStream(), encoding);
+            reader = new BufferedReader(isr);
+        }
+        return (reader);
     }
 
     public String getRemoteAddr() {
